70ce669b5ee7d859ec68406fc52f819c566ca3c5
Cédric HT <altaris@users.noreply.github.com>
2023-04-25 18:39:53 +0900

    MLC training: monitor val/f1 for eatly stopping

--- train_mlc.py
+++ train_mlc.py
@@ -1,19 +1,31 @@
+# pylint: disable=missing-function-docstring
 """Script to train acc23's current model implementation"""
-
-import sys
 
 from loguru import logger as logging
 
-from acc23.models import Bastet as Model
+from acc23.models import Ampere as Model
 from acc23.dataset import ACCDataset
 from acc23.utils import train_model
 
+
 def main():
-    name = sys.argv[1]
     ds = ACCDataset("data/train.csv", "data/images")
     train, val = ds.test_train_split_dl()
     model = Model()
-    train_model(model, train, val, root_dir="out", name=name)
+    name = model.__class__.__name__.lower()
+    train_model(
+        model,
+        train,
+        val,
+        root_dir="out",
+        name=name,
+        early_stopping_kwargs={
+            "monitor": "val/f1",
+            "patience": 10,
+            "mode": "max",
+        },
+    )
+
 
 if __name__ == "__main__":
     try:
