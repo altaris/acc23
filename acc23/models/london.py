1a0ec2bafae9c96c1619ded8faaf5f2954087bb6
Cédric HT <altaris@users.noreply.github.com>
2023-06-08 10:45:40 +0900

    Moved hparams in __init__ kwargs

--- acc23/models/london.py
+++ acc23/models/london.py
@@ -4,10 +4,11 @@ convolutional branch is a vision transformer
 """
 __docformat__ = "google"
 
-from typing import Dict, Union
+from typing import Dict, Tuple, Union
 
 import torch
 from torch import Tensor, nn
+from transformers.activations import get_activation
 
 from acc23.constants import IMAGE_SIZE, N_CHANNELS, N_FEATURES, N_TRUE_TARGETS
 
@@ -23,31 +24,39 @@ class London(BaseMultilabelClassifier):
     vision_transformer: nn.Module  # Conv. input branch
     main_branch: nn.Module  # Merge branch
 
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        n_features: int = N_FEATURES,
+        image_shape: Tuple[int, int, int] = (
+            N_CHANNELS,
+            IMAGE_SIZE,
+            IMAGE_SIZE,
+        ),
+        out_dim: int = N_TRUE_TARGETS,
+        embed_dim: int = 512,
+        patch_size: int = 16,
+        n_transformers: int = 16,
+        n_heads: int = 8,
+        dropout: float = 0.0,
+        activation: str = "gelu",
+    ) -> None:
         super().__init__()
         self.save_hyperparameters()
-        embed_dim, activation = 256, "gelu"
-        # patch_size, n_transformers, n_heads, dropout = 32, 8, 8, 0.2
-        patch_size, n_transformers, n_heads, dropout = 16, 16, 8, 0.0
-        # self.tabular_encoder = nn.Sequential(
-        #     nn.Linear(N_FEATURES, 256),
-        #     get_activation(activation),
-        #     ResidualLinearBlock(256, activation=activation),
-        #     ResidualLinearBlock(256, activation=activation),
-        #     ResidualLinearBlock(256, activation=activation),
-        #     nn.Linear(256, embed_dim),
-        #     get_activation(activation),
-        # )
-        self.tabular_encoder = linear_chain(
-            N_FEATURES,
-            [512, embed_dim],
-            activation=activation,
+        nc, s, _ = image_shape
+        self.tabular_encoder = nn.Sequential(
+            nn.Linear(n_features, embed_dim),
+            get_activation(activation),
         )
+        # self.tabular_encoder = linear_chain(
+        #     n_features,
+        #     [512, embed_dim],
+        #     activation=activation,
+        # )
         self.vision_transformer = nn.Sequential(
             nn.MaxPool2d(5, 2, 2),  # 512 -> 256
             VisionTransformer(
                 patch_size=patch_size,
-                input_shape=(N_CHANNELS, IMAGE_SIZE // 2, IMAGE_SIZE // 2),
+                input_shape=(nc, s // 2, s // 2),
                 embed_dim=embed_dim,
                 hidden_dim=2 * embed_dim,
                 out_features=embed_dim,
@@ -57,23 +66,21 @@ class London(BaseMultilabelClassifier):
                 activation=activation,
             ),
         )
-        # self.main_branch = nn.Sequential(
-        #     nn.Linear(2 * embed_dim, 256),
-        #     get_activation(activation),
-        #     ResidualLinearBlock(256, activation=activation),
-        #     ResidualLinearBlock(256, activation=activation),
-        #     ResidualLinearBlock(256, activation=activation),
-        #     nn.Linear(256, N_TRUE_TARGETS),
-        # )
-        self.main_branch = linear_chain(
-            2 * embed_dim,
-            [embed_dim, N_TRUE_TARGETS],
-            activation=activation,
-            last_activation="linear",
+        self.main_branch = nn.Sequential(
+            nn.Linear(2 * embed_dim, embed_dim),
+            get_activation(activation),
+            nn.Dropout1d(dropout),
+            nn.Linear(embed_dim, out_dim),
         )
+        # self.main_branch = linear_chain(
+        #     2 * embed_dim,
+        #     [embed_dim, out_dim],
+        #     activation=activation,
+        #     last_activation="linear",
+        # )
         self.example_input_array = (
-            torch.zeros((32, N_FEATURES)),
-            torch.zeros((32, N_CHANNELS, IMAGE_SIZE, IMAGE_SIZE)),
+            torch.zeros((32, n_features)),
+            torch.zeros((32, nc, s, s)),
         )
         self.forward(*self.example_input_array)
 
@@ -98,7 +105,11 @@ class London(BaseMultilabelClassifier):
         x = x.float().to(self.device)  # type: ignore
         img = img.to(self.device)  # type: ignore
         a = self.tabular_encoder(x)
-        b = self.vision_transformer(img)
+        b = (
+            self.vision_transformer(img)
+            # if img.max() > 0
+            # else torch.zeros_like(a)
+        )
         ab = torch.concatenate([a, b], dim=-1)
         c = self.main_branch(ab)
         return c
