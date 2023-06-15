f75fca05661bfb0bef7d1a7c02fe6bfe61849984
Cédric HT <altaris@users.noreply.github.com>
2023-06-15 10:45:52 +0900

    Model evaluation

--- acc23/__init__.py
+++ acc23/__init__.py
@@ -8,4 +8,9 @@ from .constants import *
 from .dataset import ACCDataModule, ACCDataset
 from .mlsmote import irlbl, mlsmote, remedial
 from .models import *
+from .postprocessing import (
+    eval_on_test_dataset,
+    eval_on_train_dataset,
+    output_to_dataframe,
+)
 from .preprocessing import load_csv, load_image
--- acc23/postprocessing.py
+++ acc23/postprocessing.py
@@ -6,15 +6,105 @@ __docformat__ = "google"
 
 import json
 from pathlib import Path
-from typing import Union
+from typing import Tuple, Union
 
 import pandas as pd
+import pytorch_lightning as pl
 import requests
+import torch
 from loguru import logger as logging
+from sklearn.metrics import (
+    accuracy_score,
+    f1_score,
+    precision_score,
+    recall_score,
+)
 from torch import Tensor
 
-from acc23.constants import TRUE_TARGETS
+from acc23.constants import TARGETS, TRUE_TARGETS
+from acc23.dataset import ACCDataModule
 from acc23.preprocessing import reorder_columns, set_fake_targets
+
+
+def eval_on_test_dataset(
+    model: pl.LightningModule,
+    dm: pl.LightningDataModule,
+    root_dir: Union[str, Path],
+    raw_test_csv_file_path: Union[str, Path] = "data/test.csv",
+) -> pd.DataFrame:
+    """
+    Returns a trustii-submittable dataframe containing predictions on the
+    predict dataset of the datamodule.
+
+    Args:
+        model (pl.LightningModule):
+        dm (pl.LightningDataModule):
+        root_dir (Union[str, Path]): For Pytorch Lightning use
+        raw_test_csv_file_path (Union[str, Path]): Path to the competition
+            `test.csv` (most likely `data/test.csv`). This is used to get the
+            `trustii_id` colunm.
+    """
+    trainer = pl.Trainer(
+        callbacks=[pl.callbacks.RichProgressBar()],
+        devices=1,
+        num_nodes=1,
+        default_root_dir=root_dir,
+    )
+    raw = trainer.predict(model, datamodule=dm)
+    df = output_to_dataframe(torch.cat(raw))  # type: ignore
+    df["trustii_id"] = pd.read_csv(raw_test_csv_file_path)["trustii_id"]
+    df = df[["trustii_id"] + TARGETS]  # Columns order
+    df = df.astype(int)
+    return df
+
+
+def eval_on_train_dataset(
+    model: pl.LightningModule,
+    dm: ACCDataModule,
+    root_dir: Union[str, Path],
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Returns prediction on the training+validation dataset. Also returns a
+    second dataframe containing various metrics.
+
+    Args:
+        model (pl.LightningModule):
+        dm (pl.LightningDataModule):
+        root_dir (Union[str, Path]): For Pytorch Lightning use
+    """
+    trainer = pl.Trainer(
+        callbacks=[pl.callbacks.RichProgressBar()],
+        devices=1,
+        num_nodes=1,
+        default_root_dir=root_dir,
+    )
+    # The test dataset is actually train.csv without oversampling
+    dm.prepare_data()
+    dm.setup("test")
+    raw = trainer.predict(model, dm.test_dataloader())
+    df_pred = output_to_dataframe(torch.cat(raw))  # type: ignore
+    assert dm.ds_test is not None
+    df_true = dm.ds_test.data[TARGETS]
+    df_true = df_true.where(df_true.notna(), df_pred[TARGETS])
+    n, kw = len(df_pred), {"zero_division": 0}
+    metrics = pd.DataFrame(
+        [
+            [
+                df_true[t].sum() / n,
+                df_pred[t].sum() / n,
+                f1_score(df_true[t], df_pred[t], **kw),
+                precision_score(df_true[t], df_pred[t], **kw),
+                recall_score(df_true[t], df_pred[t], **kw),
+                ((1 - df_pred[t]) * (1 - df_true[t])).sum()
+                / (1 - df_true[t]).sum(),
+                accuracy_score(df_true[t], df_pred[t]),
+            ]
+            for t in TARGETS
+        ],
+        columns=["prev_true", "prev_pred", "f1", "prec", "rec", "spec", "acc"],
+        index=TARGETS,
+    )
+    return df_pred, metrics
 
 
 def output_to_dataframe(y: Tensor) -> pd.DataFrame:
