b2867d9aea59ebebaf8f075bdb744a57887acdd0
Cédric HT <altaris@users.noreply.github.com>
2023-06-26 19:58:08 +0900

    Orchid: pretrained vit

--- acc23/models/orchid.py
+++ acc23/models/orchid.py
@@ -5,7 +5,7 @@ convolutional branch is a vision transformer and the tabular branch is a
 """
 __docformat__ = "google"
 
-from typing import Any, Dict, Tuple, Union
+from typing import Any, Dict, Literal, Tuple, Union
 
 import torch
 from torch import Tensor, nn
@@ -57,12 +57,12 @@ class TabularPreprocessor(nn.Module):
 class Orchid(BaseMultilabelClassifier):
     """See module documentation"""
 
-    tat: nn.Module  # Tabular transformer
+    mlp_head: nn.Module
     tat_pre: nn.Module  # Tabular feature preprocessing
-    vit_resize: nn.Module
+    tat: nn.Module  # Tabular transformer
     vit_proj: nn.Module
+    vit_resize: nn.Module
     vit: nn.Module  # Vision transformer
-    mlp_head: nn.Module  # Fusion branch
 
     def __init__(
         self,
@@ -73,14 +73,15 @@ class Orchid(BaseMultilabelClassifier):
         ),
         out_dim: int = N_TRUE_TARGETS,
         embed_dim: int = 512,
-        # patch_size: int = 8,
+        patch_size: int = 8,
         n_transformers: int = 16,
         n_heads: int = 8,
-        dropout: float = 0.0,
+        dropout: float = 0.1,
         activation: str = "gelu",
         mlp_dim: int = 4096,
         # pooling: bool = False,
         freeze_vit: bool = False,
+        vit: Literal["new", "pretrained", "frozen"] = "pretrained",
         **kwargs: Any,
     ) -> None:
         super().__init__(**kwargs)
@@ -99,37 +100,46 @@ class Orchid(BaseMultilabelClassifier):
             activation=activation,
             mlp_dim=mlp_dim,
         )
-        # self.vit = ViTModel(
-        #     config=ViTConfig(
-        #         hidden_size=embed_dim,
-        #         num_hidden_layers=n_transformers,
-        #         num_attention_heads=n_heads,
-        #         intermediate_size=mlp_dim,
-        #         hidden_act=activation,
-        #         hidden_dropout_prob=dropout,
-        #         attention_probs_dropout_prob=dropout,
-        #         image_size=s,
-        #         num_channels=nc,
-        #         patch_size=patch_size,
-        #     ),
-        #     add_pooling_layer=False,
-        # )
-        self.vit = ViTModel.from_pretrained("google/vit-base-patch16-224-in21k")
-        self.vit.requires_grad_(not freeze_vit)
-        if self.vit.pooler is not None:
-            self.vit.pooler.requires_grad_(False)
-        if not isinstance(self.vit, ViTModel):
-            raise RuntimeError(
-                "Pretrained ViT is not a transformers.ViTModel object"
+        if vit == "new":
+            self.vit = ViTModel(
+                config=ViTConfig(
+                    hidden_size=embed_dim,
+                    num_hidden_layers=n_transformers,
+                    num_attention_heads=n_heads,
+                    intermediate_size=mlp_dim,
+                    hidden_act=activation,
+                    hidden_dropout_prob=dropout,
+                    attention_probs_dropout_prob=dropout,
+                    image_size=s,
+                    num_channels=nc,
+                    patch_size=patch_size,
+                ),
+                add_pooling_layer=False,
             )
-        if not isinstance(self.vit.config, ViTConfig):
-            raise RuntimeError(
-                "Pretrained ViT confit is not a transformers.ViTConfig object"
+            self.vit_resize = nn.Identity()
+            self.vit_proj = nn.Identity()
+        else:
+            self.vit = ViTModel.from_pretrained(
+                "google/vit-base-patch16-224-in21k"
             )
-        self.vit_resize = Resize(self.vit.config.image_size, antialias=True)
-        self.vit_proj = nn.Linear(
-            self.vit.config.hidden_size, embed_dim, bias=False
-        )
+            self.vit.requires_grad_(not freeze_vit)
+            if self.vit.pooler is not None:
+                self.vit.pooler.requires_grad_(False)
+            if not isinstance(self.vit, ViTModel):
+                raise RuntimeError(
+                    "Pretrained ViT is not a transformers.ViTModel object"
+                )
+            if not isinstance(self.vit.config, ViTConfig):
+                raise RuntimeError(
+                    "Pretrained ViT confit is not a transformers.ViTConfig "
+                    "object"
+                )
+            self.vit_resize = Resize(
+                self.vit.config.image_size, antialias=True
+            )
+            self.vit_proj = nn.Linear(
+                self.vit.config.hidden_size, embed_dim, bias=False
+            )
         self.mlp_head = MLP(
             in_dim=2 * embed_dim,
             hidden_dims=[mlp_dim, out_dim],
@@ -153,7 +163,6 @@ class Orchid(BaseMultilabelClassifier):
         x_cat, x_num = self.tat_pre(x, self.device)
         img = img.to(self.device)  # type: ignore
         a = self.tat(x_cat, x_num)
-        # b = self.vit(img)
         b = self.vit_resize(img)
         b = self.vit(b).last_hidden_state[:, 0]
         b = self.vit_proj(b)
